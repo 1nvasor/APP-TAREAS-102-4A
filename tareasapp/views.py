bca8db0f19143dba6b75ea20b0704f69d1aced1c
Talleres Puente Alto <tallerespa@inacapmail.cl>
2026-09-24 15:42:14 -0300

    agregamos detalle y editar

--- tareasapp/views.py
+++ tareasapp/views.py
@@ -26,3 +26,26 @@ def crear_tarea(request):
         )
         return redirect('inicio')
     return render(request, 'tareasapp/crear.html')
+
+def detalle_tarea(request,id):
+    tarea = Tarea.objects.get(id=id)
+
+    return render(request,'tareasapp/detalle.html',{
+        'tarea' : tarea
+    })
+
+def editar_tarea(request,id):
+    tarea = Tarea.objects.get(id=id)
+
+    if request.method == 'POST':
+        tarea.titulo = request.POST['titulo']
+        tarea.descripcion = request.POST['descripcion']
+        tarea.completada = 'completada' in request.POST
+
+        tarea.save()
+
+        return redirect('inicio')
+
+    return render(request,'tareasapp/editar.html',{
+        'tarea':tarea
+    })
